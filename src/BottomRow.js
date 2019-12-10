afecb5154084b58b0d4731fc496122799a5fcf61
CorellCB <corellcb@gmail.com>
2019-12-10 02:26:02 -0500

    completed mvp and a stretch goal

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  let [round, setRound] = useState(0);
+  let [down, setDown] = useState(0);
+
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{down}</div>
+        <button className="quarter" onClick={() => down <= 3 ? setDown(down + 1) : setDown(down = 1)}>Down</button>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
@@ -18,7 +22,8 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+  <div className="quarter__value">{round}</div>
+        <button className="quarter" onClick={() => round <= 3 ? setRound(round + 1) : setRound(round = 1)}>Quarter</button>
       </div>
     </div>
   );
